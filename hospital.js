7800bddf8d5e75c98ca3a63e09d76d6d492c23c7
Steven Beyer <sbeyerjr@gmail.com>
2018-09-20 14:08:02 -0700

    added hospital.js

---
+++ hospital.js
@@ -0,0 +1,14 @@
+var MOCK_HOSPITALS = {
+    "hospitals": [
+        {
+            "id": "11111",
+            "name:" "Honor Health",
+            "Location": "123 Main Street"
+        },
+        {
+            "id": "22222",
+            "name": "Osborne Health",
+            "location": ""
+        }
+    ]
+}
