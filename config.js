2559d00d4a0de3a632fdf20ad728fd28a6e78907
Steven Beyer <sbeyerjr@gmail.com>
2018-09-21 10:58:10 -0700

    changed database url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/hospitality-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-hospitality-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://sbeyerjr:music123@ds159772.mlab.com:59772/blog-database';
 exports.PORT = process.env.PORT || 8080;
