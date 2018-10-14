c8eaaa93bcee0db5bd3016422969f9abe3290f61
Steven Beyer <sbeyerjr@gmail.com>
2018-10-14 12:11:57 -0700

    added login testing

---
+++ test/login.js
@@ -0,0 +1,60 @@
+'use strict';
+const { TEST_DATABASE_URL } = require('../config');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const jwt = require('jsonwebtoken');
+
+const { app, runServer, closeServer } = require('../server');
+const User  = require('../models/user');
+const { JWT_SECRET } = require('../config');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Auth endpoints', function () {
+  const username = 'exampleUser';
+  const password = 'examplePass';
+  const fullName = 'Example';
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  after(function () {
+    return closeServer();
+  });
+
+  beforeEach(function() {
+    return User.hashPassword(password).then(password =>
+      User.create({
+        username,
+        password,
+        fullName
+      })
+    );
+  });
+
+  afterEach(function () {
+    return User.remove({});
+  });
+
+  describe('/auth/login', function () {
+    it('Should return a valid auth token', function () {
+      return chai
+        .request(app)
+        .post('/auth/login')
+        .send({ username, password })
+        .then(res => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          const token = res.body.authToken;
+          expect(token).to.be.a('string');
+          const payload = jwt.verify(token, JWT_SECRET, {
+            algorithm: ['HS256']
+          });
+        
+        });
+    });
+  });
+});
